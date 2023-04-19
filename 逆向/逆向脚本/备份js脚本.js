78cc818abbe8c782b520dfda9e3d4607f9ce200c
liuguodong <liuguodong@vmos.cn>
2023-04-19 17:11:41 +0800

    1

--- 逆向/逆向脚本/备份js脚本.js
+++ 逆向/逆向脚本/备份js脚本.js
@@ -82,6 +82,14 @@ Java.perform(function () {
       console.log('ActivityonDestroy ret value is ' + ret);
       return ret;
     }  
+    Activity["finish"].overload().implementation = function () {
+      console.log(`${this.getClass()} -->Activity finish is called`);
+      let ret = this.finish();
+      console.log('Activity finish ret value is ' + ret);
+
+      printStack();
+      return ret;
+    }
   } catch (error) {
     console.log('Java.perform error:' + error);
   }
@@ -147,9 +155,209 @@ gah["createFromParcel"].implementation = function (parcel) {
   return ret;
 };
 
+// 音乐播放
+let MediaPlayer = Java.use("android.media.MediaPlayer");
+MediaPlayer["start"].implementation = function () {
+  console.log('MediaPlayer start is called');
+  let ret = this.start();
+  console.log('MediaPlayer start ret value is ' + ret);
+  return ret;
+};
+MediaPlayer["pause"].implementation = function () {
+  console.log('MediaPlayer pause is called');
+  let ret = this.pause();
+  console.log('MediaPlayer pause ret value is ' + ret);
+  return ret;
+};
+let AudioTrack = Java.use("android.media.AudioTrack");
+AudioTrack["pause"].implementation = function () {
+  console.log('AudioTrack pause is called');
+  let ret = this.pause();
+  console.log('AudioTrack pause ret value is ' + ret);
 
+  printStack();
+  return ret;
+};
 
+// 哪个控件点击了
+let View = Java.use("android.view.View");
+View["performClick"].implementation = function () {
+  let Parent1 = this.getParent();
+  // var Color = Java.use('android.graphics.Color');
+  // Parent1.setBackgroundColor(Color.argb(127, 2, 2, 2));
+  console.log('View performClick is called getClass：'+this.getClass()
+  + ', ' + 'Parent1: ' + Parent1
+  );
+  let ret = this.performClick();
+  console.log('View performClick ret value is ' + ret);
+  return ret;
+};
 
+// 播放器
+let AudioManager = Java.use("android.media.AudioManager");
+AudioManager["requestAudioFocus"].overload('android.media.AudioFocusRequest').implementation = function (build2) {
+  console.log('AudioManager requestAudioFocus is called' + ', ' + 'build2: ' + build2);
+  let ret = this.requestAudioFocus(build2);
+  console.log('AudioManager requestAudioFocus ret value is ' + ret);
+  return ret;
+};
+let AudioDriver = Java.use("com.spotify.playback.playbacknative.AudioDriver");
+AudioDriver["setPaused"].implementation = function (z) {
+  console.log('AudioDriver setPaused is called' + ', ' + 'z: ' + z);
+  let ret = this.setPaused(z);
+  console.log('AudioDriver setPaused ret value is ' + ret);
+  return ret;
+};
+AudioDriver["addListener"].implementation = function (audioDriverListener) {
+  console.log('AudioDriver addListener is called' + ', ' + 'audioDriverListener: ' + audioDriverListener);
+  let ret = this.addListener(audioDriverListener);
+  console.log('AudioDriver addListener ret value is ' + ret);
 
+  printStack();
+  return ret;
+};
+AudioDriver["removeListener"].implementation = function (audioDriverListener) {
+  console.log('AudioDriver removeListener is called' + ', ' + 'audioDriverListener: ' + audioDriverListener);
+  let ret = this.removeListener(audioDriverListener);
+  console.log('AudioDriver removeListener ret value is ' + ret);
+  return ret;
+};
+AudioDriver["stopDuckingAudioSession"].overload('int').implementation = function (i) {
+  console.log('AudioDriver stopDuckingAudioSession is called' + ', ' + 'i: ' + i);
+  let ret = this.stopDuckingAudioSession(i);
+  console.log('AudioDriver stopDuckingAudioSession ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+AudioDriver["destroy"].implementation = function () {
+  console.log('AudioDriver destroy is called');
+  let ret = this.destroy();
+  console.log('AudioDriver destroy ret value is ' + ret);
+  return ret;
+};
+AudioDriver["onMarkerReached"].implementation = function (audioTrack) {
+  console.log('AudioDriver onMarkerReached is called' + ', ' + 'audioTrack: ' + audioTrack);
+  let ret = this.onMarkerReached(audioTrack);
+  console.log('AudioDriver onMarkerReached ret value is ' + ret);
+  return ret;
+};
+AudioDriver["startPlayback"].implementation = function () {
+  console.log('AudioDriver startPlayback is called');
+  let ret = this.startPlayback();
+  console.log('AudioDriver startPlayback ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+AudioDriver["stopPlayback"].implementation = function () {
+  console.log('AudioDriver stopPlayback is called');
+  let ret = this.stopPlayback();
+  console.log('AudioDriver stopPlayback ret value is ' + ret);
+  return ret;
+};
+AudioDriver["open"].overload('int', 'int', 'int', 'int').implementation = function (i, i2, i3, i4) {
+  console.log('AudioDriver open is called' + ', ' + 'i: ' + i + ', ' + 'i2: ' + i2 + ', ' + 'i3: ' + i3 + ', ' + 'i4: ' + i4);
+  let ret = this.open(i, i2, i3, i4);
+  console.log('AudioDriver open ret value is ' + ret);
+  return ret;
+};
+AudioDriver["getCurrentAudioSession"].implementation = function () {
+  console.log('AudioDriver getCurrentAudioSession is called');
+  let ret = this.getCurrentAudioSession();
+  console.log('AudioDriver getCurrentAudioSession ret value is ' + ret);
+  printStack();
+  return ret;
+};
+let AudioTrack = Java.use("android.media.AudioTrack");
+AudioTrack["pause"].implementation = function () {
+  console.log('AudioTrack pause is called');
+  let ret = this.pause();
+  console.log('AudioTrack pause ret value is ' + ret);
+  return ret;
+};
+
+let MediaPlayer = Java.use("android.media.MediaPlayer");
+MediaPlayer["start"].implementation = function () {
+  console.log('MediaPlayer start is called');
+  let ret = this.start();
+  console.log('MediaPlayer start ret value is ' + ret);
+  return ret;
+};
+MediaPlayer["pause"].implementation = function () {
+  console.log('MediaPlayer pause is called');
+  let ret = this.pause();
+  console.log('MediaPlayer pause ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+let MediaPlayer = Java.use("android.media.MediaPlayer");
+MediaPlayer["start"].implementation = function () {
+  console.log('MediaPlayer start is called');
+  let ret = this.start();
+  console.log('MediaPlayer start ret value is ' + ret);
+  return ret;
+};
+MediaPlayer["pause"].implementation = function () {
+  console.log('MediaPlayer pause is called');
+  let ret = this.pause();
+  console.log('MediaPlayer pause ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+
+//电量管理
+let PowerManager = Java.use("android.os.PowerManager");
+PowerManager["isDeviceIdleMode"].implementation = function () {
+  console.log('PowerManager isDeviceIdleMode is called');
+  let ret = this.isDeviceIdleMode();
+  console.log('PowerManager isDeviceIdleMode ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+PowerManager["isPowerSaveMode"].implementation = function () {
+  console.log('PowerManager isPowerSaveMode is called');
+  let ret = this.isPowerSaveMode();
+  console.log('PowerManager isPowerSaveMode ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+let PowerManagerWakeLock = Java.use("android.os.PowerManager$WakeLock");
+PowerManagerWakeLock["setReferenceCounted"].implementation = function (bbfVar) {
+  console.log('PowerManagerWakeLock setReferenceCounted is called' + ', ' + 'bbfVar: ' + bbfVar);
+  let ret = this.setReferenceCounted(bbfVar);
+  console.log('PowerManagerWakeLock setReferenceCounted ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+PowerManagerWakeLock["acquire"].overload().implementation = function () {
+  console.log('PowerManagerWakeLock acquire is called');
+  let ret = this.acquire();
+  console.log('PowerManagerWakeLock acquire ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+PowerManagerWakeLock["acquire"].overload('long').implementation = function (bbfVar) {
+  console.log('PowerManagerWakeLock acquire is called' + ', ' + 'bbfVar: ' + bbfVar);
+  let ret = this.acquire(bbfVar);
+  console.log('PowerManagerWakeLock acquire ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
+PowerManagerWakeLock["release"].overload('int').implementation = function (bbfVar) {
+  console.log('PowerManagerWakeLock release is called' + ', ' + 'bbfVar: ' + bbfVar);
+  let ret = this.release(bbfVar);
+  console.log('PowerManagerWakeLock release ret value is ' + ret);
+
+  printStack();
+  return ret;
+};
 
 
